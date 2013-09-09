e86a2aae1079c2ce0299317282f1c3607a919a2e
trinhluu33@gmail.com <trinhluu33@gmail.com@075815e7-a533-d1ca-b97d-920d00a307ac>
2013-09-09 14:52:49 +0000

    Add changes to QueryBLL prj

--- my projects/BusinessLayer/FzQueryBLL.cs
+++ my projects/BusinessLayer/FzQueryBLL.cs
@@ -7,5 +7,61 @@ namespace BusinessLayer
 {
     public class FzQueryBLL
     {
+         #region 1. Fields
+
+        private String _queryName;//Each query has a name
+        private String _queryString;// And each queryName contents a string query
+
+        #endregion
+
+        #region 2. Properties
+
+        public String QueryName
+        {
+            get { return _queryName; }
+            set { _queryName = value; }
+        }
+
+        public String QueryString
+        {
+            get { return _queryString; }
+            set { _queryString = value; }
+        }
+
+        #endregion
+
+        #region 3. Contructors
+
+        public FzQueryBLL()
+        {
+            this._queryName = String.Empty;
+            this._queryString = String.Empty;
+        }
+
+        public FzQueryBLL(String queryString)
+        {
+            this._queryName = String.Empty;
+            this._queryString = queryString;
+        }
+
+        public FzQueryBLL(String queryName, String queryString)
+        {
+            this._queryName = queryName;
+            this._queryString = queryString;
+        }
+
+        #endregion
+
+        #region 4. Methods
+
+
+       
+        #endregion
+
+        #region 5. Privates
+
+
+
+        #endregion
     }
 }
