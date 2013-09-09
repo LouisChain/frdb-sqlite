d7530cb3a04d06422ff8c4bd3e2c88e307c7a728
trinhluu33@gmail.com <trinhluu33@gmail.com@075815e7-a533-d1ca-b97d-920d00a307ac>
2013-09-09 14:48:13 +0000

    Save changes to FzSchemeBLL.cs

--- my projects/BusinessLayer/FzSchemeBLL.cs
+++ my projects/BusinessLayer/FzSchemeBLL.cs
@@ -7,5 +7,61 @@ namespace BusinessLayer
 {
     public class FzSchemeBLL
     {
+        #region 1. Fields
+
+        private String _schemeName;
+        private List<FzAttributeBLL> _attributes; //List of attributes of scheme
+
+        #endregion
+
+        #region 2. Properties
+
+        public String SchemeName
+        {
+            get { return _schemeName; }
+            set { _schemeName = value; }
+        }
+
+        public List<FzAttributeBLL> Attributes
+        {
+            get { return _attributes; }
+            set { _attributes = value; }
+        }
+
+        #endregion
+
+        #region 3. Contructors
+
+        public FzSchemeBLL()
+        {
+            this._schemeName = String.Empty;
+            this._attributes = new List<FzAttributeBLL>();
+        }
+
+        public FzSchemeBLL(String schemeName, List<FzAttributeBLL> attributes)
+        {
+            this._schemeName = schemeName;
+            this._attributes = attributes;
+        }
+
+        public FzSchemeBLL(String schemeName)
+        {
+            this._schemeName = schemeName;
+            this._attributes = new List<FzAttributeBLL>();
+        }
+
+        #endregion
+
+        #region 4. Methods
+
+
+       
+        #endregion
+
+        #region 5. Privates
+
+
+
+        #endregion
     }
 }
