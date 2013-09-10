6d0ba0d26af605d7392da8f96a8fb5f06eb55e57
trinhluu33@gmail.com <trinhluu33@gmail.com@075815e7-a533-d1ca-b97d-920d00a307ac>
2013-09-10 16:16:53 +0000

    Add all file basic to BLL proj

---
+++ bin/FRDB-SQLite/BusinessLayer/FzDataTypeBLL.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class FzDataTypeBLL
+    {
+        #region 1. Fields
+
+        private String _typeName;// TypeName != DataType if DataType == "User-Defined"
+        private String _dataType;
+        private String _domainString;
+        private List<String> _domainValues;
+
+        #endregion
+
+        #region 2. Properties
+
+        public String TypeName 
+        {
+            get { return _typeName; }
+            set { _typeName = value; }
+        }
+
+        public String DataType
+        {
+            get { return _dataType; }
+            set { _dataType = value; }
+        }
+
+        public String DomainString
+        {
+            get { return _domainString; }
+            set { _domainString = value; }
+        }
+
+        public List<String> DomainValues
+        {
+            get { return _domainValues; }
+            set { _domainValues = value; }
+        }
+
+        #endregion
+
+        #region 3. Contructors
+
+        public FzDataTypeBLL()
+        {
+            this._typeName = String.Empty;
+            this._dataType = String.Empty;
+            this._domainString = String.Empty;
+            this._domainValues = new List<String>();
+        }
+
+        public FzDataTypeBLL(String typeName, String domain)
+        {
+            this._typeName = typeName;
+            this._domainString = domain;
+            GetDomain(domain);
+            this._dataType = "UserDefined";
+            GetDataType();
+        }
+
+        #endregion
+
+        #region 4. Methods
+
+        #endregion
+
+        #region 5. Privates
+
+        private void GetDomain(String domain)
+        {
+            try
+            {
+                if (this._typeName == "UserDefined")
+                {
+                    domain = domain.Replace("{", "");
+                    domain = domain.Replace("}", "");
+                    char[] seperator = { ',' };
+                    String[] temp = domain.Split(seperator);
+                    _domainValues = new List<String>();
+
+                    foreach (String value in temp)
+                    {
+                        _domainValues.Add(value.Trim());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR:\n" + ex.Message);
+            }
+        }
+
+        private void GetDataType()
+        {
+            try
+            {
+                switch (this._typeName)
+                {
+                    case "Int16": this._dataType = "Int16"; break;
+                    case "Int64": this._dataType = "Int64"; break;
+                    case "Int32": this._dataType = "Int32"; break;
+                    case "Byte": this._dataType = "Byte"; break;
+                    case "Decimal": this._dataType = "Decimal"; break;
+                    case "Currency": this._dataType = "Currency"; break;
+                    case "String": this._dataType = "String"; break;
+                    case "DateTime": this._dataType = "DateTime"; break;
+                    case "Binary": this._dataType = "Binary"; break;
+                    case "Single": this._dataType = "Single"; break;
+                    case "Double": this._dataType = "Double"; break;
+                    case "Boolean": this._dataType = "Boolean"; break;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERROR:\n" + ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ bin/FRDB-SQLite/BusinessLayer/FzRelationBLL.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class FzRelationBLL
+    {
+         #region 1. Fields
+
+        private String _relationName;
+        private FzSchemeBLL _scheme;//Each relation has a respective scheme
+        private List<FzTupleBLL> _tuples;//List of tuples
+        #endregion
+
+        #region 2. Properties
+
+        public String RelationName
+        {
+            get { return _relationName; }
+            set { _relationName = value; }
+        }
+
+        public FzSchemeBLL Scheme
+        {
+            get { return _scheme; }
+            set { _scheme = value; }
+        }
+
+        public List<FzTupleBLL> Tuples
+        {
+            get
+            {
+                //List<FzTupleEntity> result = new List<FzTupleEntity>();
+                //foreach (FzTupleEntity item in this._tuples)
+                //{
+                //    FzTupleEntity tmp = new FzTupleEntity(item);
+                //    result.Add(tmp);
+                //}
+                //return result;
+                return _tuples;
+            }
+            set
+            {
+                foreach (FzTupleBLL item in value)
+                {
+                    FzTupleBLL tmp = new FzTupleBLL(item);
+                    _tuples.Add(tmp);
+                }
+                //_tuples = value;
+            }
+        }
+
+        #endregion
+
+        #region 3. Contructors
+
+        public FzRelationBLL()
+        {
+            this._relationName = String.Empty;
+            this._scheme = new FzSchemeBLL();
+            this._tuples = new List<FzTupleBLL>();
+        }
+
+        public FzRelationBLL(String relationName)
+        {
+            this._relationName = relationName;
+            this._scheme = new FzSchemeBLL();
+            this._tuples = new List<FzTupleBLL>();
+        }
+
+        public FzRelationBLL(FzRelationBLL old)
+        {
+            this._relationName = old._relationName;
+            this._scheme = old._scheme;
+            this._tuples = old._tuples;
+
+        }
+        #endregion
+
+        #region 4. Methods (none)
+
+        #endregion
+
+        #region 5. Privates (none)
+        #endregion
+    }
+}
