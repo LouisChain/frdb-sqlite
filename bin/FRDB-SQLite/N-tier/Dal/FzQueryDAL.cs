b443d66fdaab76e26193e2395cc88ab002856f25
trinhluu33@gmail.com <trinhluu33@gmail.com@075815e7-a533-d1ca-b97d-920d00a307ac>
2013-09-13 16:10:23 +0000

    Save changes FzQueryDAL.cs is OK

--- bin/FRDB-SQLite/N-tier/Dal/FzQueryDAL.cs
+++ bin/FRDB-SQLite/N-tier/Dal/FzQueryDAL.cs
@@ -8,6 +8,27 @@ namespace N_tier
 {
     public class FzQueryDAL
     {
-        
+        public static List<String> ListOfQueryName(FzDbBLL fdb)
+        {
+            List<String> queryNames = new List<String>();
+            foreach (FzQueryBLL query in fdb.Queries)
+            {
+                queryNames.Add(query.QueryName);
+            }
+
+            return queryNames;
+        }
+
+        public static FzQueryBLL GetQueryByName(String queryName, FzDbBLL fdb)
+        {
+            foreach (var item in fdb.Queries)
+            {
+                if (queryName.Equals(item.QueryName))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
     }
 }
